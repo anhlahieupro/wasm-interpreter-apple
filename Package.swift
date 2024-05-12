6ad1f4cd868d171cd048b6e0949f6bc2d6c08f54
dev <--->
2024-05-12 21:46:51 +0700

    update platforms

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "WasmInterpreter",
     platforms: [
-        .macOS(.v11), .iOS(.v14),
+        .macOS(.v10_15),
     ],
     products: [
         .library(
